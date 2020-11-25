5adabd75d4c6ce0003a16616d4ef7f2171595719
dante <dantecherng@hotmail.com.tw>
2020-11-26 04:51:47 +0800

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,47 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path, notice: "新增成功"
+    else
+      render :new, notice: "新增失敗"
+    end
+  end
+
+  def show
+    @book = Book.find(prams[:id])
+  end
+
+  def edit
+    @book = Book.find(prams[:id])
+  end
+
+  def update
+    @book = Book.find(prams[:id])
+    if @book.update(book_params)
+      redirect_to books_path, notice: "修改成功"
+    else
+      render :edit, notice: "修改失敗"
+    end
+  end
+
+  def destroy
+    @book = Book.find(prams[:id])
+    @book.destroy
+    redirect_to books_path, notice: "刪除成功"
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :price)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validate :name ,presence: true , length: { minimum: 4}
+  validate :price ,presence: true , numericality: { greater_than: 0}
+
 end
